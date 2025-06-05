0d32a732d5eb9e330d3ead0ea9d906e709c47f74
gdghanu-nguyentrinhvo <gdschanu.vont0101@gmail.com>
2025-06-05 14:12:09 +0700

    entity

---
+++ Google_Login/src/main/java/com/example/Google_Login/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.Google_Login.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+        @Id
+        private String email;
+        private String username;
+}
